8e4a5d6febf71e464ff83af3c06a1582001e116d
docdork <97050042+docdork@users.noreply.github.com>
2024-10-27 14:12:24 +0000

    build restful API

--- restfulapi.cjs
+++ restfulapi.cjs
@@ -2,11 +2,15 @@ const express = require("express");
 const uuid = require("uuid");
 const app = express();
 
+// CRUD = create, read, update, delete/destroy
+
+// Define a memory database
 let data = [
   { id: uuid(), name: "Bob" },
   { id: uuid(), name: "Alice" },
 ];
 
+// create a model for CRUD functions
 const usr = {
   create(name) {
     const user = { id: uuid(), name };
@@ -29,4 +33,25 @@ const usr = {
   },
 };
 
+//request handler for post method
+app.post("/users/:name", (req, res) => {
+  res.status(201).json(usr.create(req.params.name));
+});
 
+// request handler for get method
+app.get("/users/:id", (req, res) => {
+  res.status(200).json(usr.read(req.params.id));
+});
+
+// put request handler, an Update operation.  ID needs to be provided in order to update the specific user in the data array.
+app.put("/users/:id=:name", (req, res) => {
+  res.status(200).json(usr.update(req.params.id, req.params.name));
+});
+
+// request handler for the delete operation
+app.delete("/user/:id", (req, res) => {
+  res.status(200).json(usr.delete(req.params.id));
+});
+
+// satrt app from port 1337
+app.listen(1337, () => console.log("Web Server running on port 1337"));
